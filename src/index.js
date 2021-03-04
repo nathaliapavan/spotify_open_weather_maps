b5fe8e9e46ba4605a5e05c8181161e8af01fba6e
Nathalia Pavan <nanahpavan@gmail.com>
2021-03-04 18:43:24 -0300

    Refatoração + Pega cep do form

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 const express = require("express");
 
 const viaCepService = require("./service/ViaCepService");
-const  openWeatherMapsService = require("./service/OpenWeatherMapsService");
-const  spotifyService = require("./service/SpotifyService");
+const openWeatherMapsService = require("./service/OpenWeatherMapsService");
+const spotifyService = require("./service/SpotifyService");
 
+/*
 spotifyService.authorize()
     .then( data => {
         spotifyService.getData(data.access_token)
             .then(response => { console.log(response.data) })
     });
-
-
-//const city = viaCepService.getCity(17010010)
-const city = viaCepService.getCity(18680590)
-            .then(cidade => {
-                openWeatherMapsService.getData(cidade).then(response => console.log(response.cod));
-            });
+*/
 
 const app = express();
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
 
+app.get('/playlist', function (req, res) {
+    const cep = req.query.cep;
+    viaCepService.getCity(cep)
+    .then(cidade => {
+        openWeatherMapsService.getData(cidade)
+            .then(response => {
+                console.log(response.main)
+            });
+    });
+    res.send(`Cep enviado ${cep}`);
+});
+
 app.listen(3333, () => {
-    console.log('🚀 spotifyOpenWeatherMaps Started!');
+    console.log('🚀 SpotifyOpenWeatherMaps Started!');
 });
